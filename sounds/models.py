828864dd3f5c56b7f932623e2a952de2064a915c
Gaëtan Caillaut <gaetan.caillaut@live.com>
2017-07-20 01:19:30 +0200

    The description field can be blank

--- sounds/models.py
+++ sounds/models.py
@@ -7,7 +7,7 @@ class Sound(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.FileField(upload_to='sounds')
     title = models.CharField(max_length=32)
-    description = models.CharField(max_length=255)
+    description = models.TextField(blank=True, max_length=255)
 
     def get_absolute_url(self):
         return reverse('sounds:detail', args=[self.pk])
